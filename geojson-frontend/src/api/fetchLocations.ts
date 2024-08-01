790b2c3b10a3f765544420e2bfdfdc5ebeeea25b
amirazer55 <amirazer555@gmail.com>
2024-08-01 22:58:00 +0300

    containerize backend and front end and create docker compe as orchestrator for handling front end and back end

--- geojson-frontend/src/api/fetchLocations.ts
+++ geojson-frontend/src/api/fetchLocations.ts
@@ -1,7 +1,7 @@
 
 export const fetchLocations = async () => {
   const response = await fetch(
-    `http://localhost:3015/locations?page=${1}&limit=${10}`
+    `http://localhost:3044/locations?page=${1}&limit=${10}`
   );
   if (!response.ok) {
     throw new Error("Network response was not ok");
