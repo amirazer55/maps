b44d45da9fb21a65f690714677ea41cb64430011
amirazer55 <amirazer555@gmail.com>
2024-08-02 17:54:29 +0300

    adjust api url

--- geojson-frontend/src/api/fetchLocations.ts
+++ geojson-frontend/src/api/fetchLocations.ts
@@ -1,9 +1,8 @@
-const API_BASE_URL =
-  "https://testhell-hgksan5dj-amirazers-projects.vercel.app";
+const API_BASE_URL = "https://testhell-qqw.vercel.app";
 
 export const fetchLocations = async (page = 1, limit = 10) => {
   const response = await fetch(
-    `${API_BASE_URL}/api/locations?page=${page}&limit=${limit}`
+    `${API_BASE_URL}/locations?page=${page}&limit=${limit}`
   );
   if (!response.ok) {
     throw new Error("Network response was not ok");
